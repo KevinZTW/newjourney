4db3c767fd965f30e1d50377259303febab05dbc
Kevin Zhang <kai821104@gmail.com>
2020-06-07 17:27:00 +0800

    5:@6 pm

--- app.js
+++ app.js
@@ -5,31 +5,44 @@ const app = express();
 const today = new Date()
 const currentDay = today.getDay()
 app.set('view engine', 'ejs');
+let Items = ["Buy food", "Cook food"];
+let var1 =""
+
+
+
+var options = {
+    weekday: "long",
+    day : "numeric",
+    month :"long"
+}
+var day = today.toLocaleDateString("en-US", options)
+switch(currentDay){
+    
+    case 0:
+        var dayname = "Sunday";
+        break;
+    case 1:
+        var dayname = "Monday";
+        break;
+    case 2:
+        var dayname = "Tuesday";
+        break;}
 
 
 app.use(bodyParser.urlencoded({extneded:true}))
 
+
+
+
 app.get("/", function(req, res){
-   res.sendFile(__dirname+"/index.html");
+   res.render("list", {kindDay :dayname+day, var1:Items});
+   
 }); 
 
 app.post("/", function(req, res){
-    console.log(currentDay);
-    switch(currentDay){
-    
-        case 0:
-            var dayname = "Sunday";
-            break;
-        case 1:
-            var dayname = "Monday";
-            break;
-        case 2:
-            var dayname = "Tuesday";
-            break;
-    
-    }
-   
-    res.render("list", {kindDay :dayname});
+    Item = req.body.newlist
+    Items.push(Item)   
+    res.redirect("/");
 });
 
 
